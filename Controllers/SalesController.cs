8241c1e5077ca7df19294953a4de9812c324fd93
Ryan Gilbert <u22507966@tuks.co.za>
2026-06-01 20:39:51 +0200

    - New sales endpoint called getDates which returns all dates from sales, excluding todays date. - Not working correctly, still returns ALL dates when i only want distinct dates. Probably because dates also have time variables which are always going to be different

--- Controllers/SalesController.cs
+++ Controllers/SalesController.cs
@@ -158,5 +158,18 @@ namespace RoadStallAPI.Controllers
             
             return totalUnitsSold;
         }
+
+        [HttpGet("getDates")]
+        public async Task<ActionResult<DateOnly[]>> getDates()
+        {
+            var today = DateTime.Today;
+            var dates = await _context.Sale.Select(s => s.Date).Where(s => s.Date < today).Distinct().ToListAsync();
+            if (dates.Count == 0)
+            {
+                return NotFound(new {message = "No dates with historical data found"});
+            }
+
+            return Ok(dates);
+        }
     }
 }
